f38d2541d89388d69dd2ab670e84850f8896d8dc
Robert Toiu <toiurobert20@gmail.com>
2026-06-27 15:28:46 +0300

    fix conftest.py

---
+++ conftest.py
@@ -0,0 +1,35 @@
+
+import os
+import pytest
+from playwright.sync_api import sync_playwright
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser-name",
+        action="store",
+        default="chromium",
+        choices=["chromium", "firefox", "webkit"],
+        help="Browser to run tests with"
+    )
+
+
+@pytest.fixture(scope="function")
+def page(request):
+    browser_name = request.config.getoption("--browser-name")
+    headless = os.getenv("CI", "false").lower() == "true"
+    with sync_playwright() as p:
+        if browser_name == "chromium":
+            browser = p.chromium.launch(headless=headless)
+        elif browser_name == "firefox":
+            browser = p.firefox.launch(headless=headless)
+        elif browser_name == "webkit":
+            browser = p.webkit.launch(headless=headless)
+        else:
+            raise ValueError(f"Unsupported browser: {browser_name}")
+        
+        page = browser.new_page()
+
+        yield page
+        
+        page.close()
+        browser.close()
